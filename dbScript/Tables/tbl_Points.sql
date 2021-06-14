0b34cecd8ee53147b93e30e9c47aa24fd94ce370
236ltAWin10 <developer.tigor@gmail.com>
2021-06-14 10:31:10 +0530

    Added Points Schema.

---
+++ dbScript/Tables/tbl_Points.sql
@@ -0,0 +1,17 @@
+CREATE TABLE [dbo].[tbl_Points](
+	[distroId] [int] NOT NULL,
+	[Date] [datetime] NULL,
+	[GoogleTrendsPoints] [decimal](18, 2) NULL,
+	[GoogleTrendsPointsFinal] [decimal](18, 2) NULL,
+	[GoogleTrendsWeightage] [decimal](18, 2) NULL,
+	[DistroWatchPoints] [decimal](18, 2) NULL,
+	[DistroWatchPointsFinal] [decimal](18, 2) NULL,
+	[DistroWatchWeightage] [decimal](18, 2) NULL,
+	[TotalPoints] [decimal](18, 2) NULL
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[tbl_Points] ADD  CONSTRAINT [DF_tbl_Points_Date]  DEFAULT (getdate()) FOR [Date]
+GO
+
+
