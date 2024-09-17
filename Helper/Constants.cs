a2cd33e57b81cda7f6bc0749fdc68183d1c196b4
Nikhil Win11 Desktop <developer.tigor@gmail.com>
2024-09-17 09:52:16 +0530

    Project Name moved to constant

--- Helper/Constants.cs
+++ Helper/Constants.cs
@@ -4,5 +4,6 @@
     {
         public const string URL_PARAMETER_DISTRO_CODE = "distroCode";
         public const string URL_PARAMETER_DISTRO_ID = "distroId";
+        public const string PROJECT_NAME = "Linux Distros";
     }
 }
