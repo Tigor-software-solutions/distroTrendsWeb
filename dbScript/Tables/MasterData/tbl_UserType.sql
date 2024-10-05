55de198bb95a1780dedc5a16134c79391d6eacb6
Nikhil Win11 Desktop <developer.tigor@gmail.com>
2024-10-05 23:31:08 +0530

    DT-16 : UserType table schema with master data.

---
+++ dbScript/Tables/MasterData/tbl_UserType.sql
@@ -0,0 +1,39 @@
+INSERT INTO [dbo].[tbl_UserType]
+           ([Cd]
+           ,[Name]
+           ,[Description])
+     VALUES
+           ("SD"
+           ,"Software Developer"
+           ,"Software Developer")
+GO
+
+INSERT INTO [dbo].[tbl_UserType]
+           ([Cd]
+           ,[Name]
+           ,[Description])
+     VALUES
+           ("HU"
+           ,"Home User"
+           ,"Home User")
+GO
+
+INSERT INTO [dbo].[tbl_UserType]
+           ([Cd]
+           ,[Name]
+           ,[Description])
+     VALUES
+           ("S"
+           ,"Student"
+           ,"Student")
+GO
+
+INSERT INTO [dbo].[tbl_UserType]
+           ([Cd]
+           ,[Name]
+           ,[Description])
+     VALUES
+           ("Pro"
+           ,"Professional"
+           ,"Professional")
+GO
