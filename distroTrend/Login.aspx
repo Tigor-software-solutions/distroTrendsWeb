34334ef0c21889531cf5c6c0d85a5b6d91d2bd0d
Developer Tigor <developer.tigor@gmail.com>
2025-10-19 20:02:54 +0530

    DT-32 - Google Login added code and pages.

---
+++ distroTrend/Login.aspx
@@ -0,0 +1,14 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="distroTrend.Loign" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Login</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:Button ID="GoogleLoginButton" runat="server" Text="Login with Google" OnClick="GoogleLoginButton_Click" />
+    </form>
+</body>
+</html>
---
+++ distroTrend/Login.aspx.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Google;
+using System;
+using System.Web;
+
+namespace distroTrend
+{
+    public partial class Loign : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void GoogleLoginButton_Click(object sender, EventArgs e)
+        {
+            HttpContext.Current.GetOwinContext().Authentication.Challenge(new AuthenticationProperties
+            {
+                RedirectUri = "/Default.aspx"
+            }, "Google");
+        }
+    }
+}
